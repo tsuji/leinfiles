602e14101a68bf57cdca6b7feab0bc75e9be9451
Hironobu Tsuji <Hironobu.Tsuji@gmail.com>
2013-02-05 18:50:44 +0900

    nreplでtableモジュールがうまく動かないので、とりあえず無効化

--- profiles.clj
+++ profiles.clj
@@ -8,12 +8,13 @@
              ]
    :repl-options {
      :init (do
-       (use '[table.core :only (table)])
-       (use 'desc.core)
-       (use 'rubydoc.core)
+;;       (use '[table.core :only (table)])
+;;       (use 'desc.core)
+;;       (use 'rubydoc.core)
        (use '[clojure.repl :only (dir-fn)])
-       (load-file (str (System/getProperty "user.home") "/.lein/user.clj"))) }
-   :dependencies [[table "0.3.2"] [desc "0.1.0"]
-                  [rubydoc "0.3.0"]]
+       ;; (load-file (str (System/getProperty "user.home") "/.lein/user.clj"))
+       ) }
+;;   :dependencies [[table "0.3.2"] [desc "0.1.0"]
+;;                  [rubydoc "0.3.0"]]
  }
 }
