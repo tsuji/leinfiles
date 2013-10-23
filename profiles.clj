8e80e2e8fc3d01e3580a14d41eb166ca10ab10ec
Hironobu Tsuji <Hironobu.Tsuji@gmail.com>
2013-10-23 12:14:33 +0900

    tableを復活 (emacs の setenv COLUMNと合わせてね)。ドキュメントシステムとして marginalia を使ってみることにする。lein marg でHTML生成。

--- profiles.clj
+++ profiles.clj
@@ -1,20 +1,34 @@
-{
- :user {
-   :plugins [
-             ;[lein-clojars "0.9.0"]
-             ;[lein-light "0.0.4"]
-             [lein-deps-tree "0.1.2"]
-             [lein-difftest "2.0.0"]
-             ]
-   :repl-options {
-     :init (do
-;;       (use '[table.core :only (table)])
-;;       (use 'desc.core)
-;;       (use 'rubydoc.core)
-       (use '[clojure.repl :only (dir-fn)])
-       ;; (load-file (str (System/getProperty "user.home") "/.lein/user.clj"))
-       ) }
-;;   :dependencies [[table "0.3.2"] [desc "0.1.0"]
-;;                  [rubydoc "0.3.0"]]
+{:user {
+        :plugins [
+                  [lein-marginalia "0.7.1"]
+                  [lein-deps-tree "0.1.2"]
+                  [lein-ritz "0.7.0"]
+                  [lein-ring "0.8.7"]
+                  [org.clojars.hozumi/hiccup-bridge "1.0.0-SNAPSHOT"]
+                  ;; from cldwalker / leinfiles
+                  [lein-difftest "2.0.0"]
+                  [lein-grep "0.1.0"]
+                  [lein-open "0.1.0"]
+                  [lein-outdated "1.0.0"]
+                  [lein-exec "0.3.0"]
+                  ;;[lein-pprint "1.1.1"]
+                  ;;[lein-spell "0.1.0"]
+                  ]
+        :repl-options {
+                       :nrepl-middleware [ritz.nrepl.middleware.javadoc/wrap-javadoc
+                                          ritz.nrepl.middleware.simple-complete/wrap-simple-complete]
+                       :init (do
+                               ;;(use '[table.core :only (table)])
+                               ;;(use 'desc.core)
+                               ;;(use 'rubydoc.core)
+                               (use '[clojure.repl :only (dir-fn)])
+                               (load-file (str (System/getProperty "user.home") "/.lein/user.clj"))
+                               ) }
+        :dependencies [[ritz/ritz-nrepl-middleware "0.7.0"]
+                       [table "0.4.0"]
+                       ;;[desc "0.1.0"]
+                       ;;[rubydoc "0.3.0"]
+                       ]
+        }
+ ;; :hooks [ritz.add-sources]
  }
-}
